816b1d6b6363172be4d641c5e02f326cb4841801
Remi Debruyne <remidebruyne@hotmail.com>
2024-05-17 14:42:04 +0200

    minimum

--- minimum.js
+++ minimum.js
@@ -10,6 +10,8 @@
 
 // ```
 
+//Technically useless answer since Math.min() is already a function.
+
 function findMinNumber(...parameter) {
   result = Math.min(...parameter);
   return result;
@@ -17,3 +19,12 @@ function findMinNumber(...parameter) {
 
 findMinNumber(-8, 2, 3, 6);
 console.log(result);
+
+//This was the expected answer
+function minNumber(a, b) {
+  if (a > b) {
+    return b;
+  } else {
+    return a;
+  }
+}
